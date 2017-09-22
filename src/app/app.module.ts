5c011f34c074a25afdddf3e68c36464e1fcd797c
mahesh <mahs.nayak@gmail.com>
2017-09-22 19:48:46 +0530

    hello-component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponentComponent } from './hello-component/hello-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponentComponent
   ],
   imports: [
     BrowserModule
